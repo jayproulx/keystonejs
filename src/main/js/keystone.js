bae591e96aef5b031dc61053d870b1ee160188fc
Jay Proulx <jason.proulx@pollensoft.com>
2012-11-21 17:30:31 -0500

    Not yet stable

--- src/main/js/keystone.js
+++ src/main/js/keystone.js
@@ -3,7 +3,17 @@
  *
  * Keystone is meant to provide enough code to standardize on implementation patterns, without requiring any other
  * dependencies.  Using Keystone should allow the developer to standardize on Keystone patterns on a project without
- * being required to use all of the
+ * being required to use all of the extra bells and whistles that you'll find in Arch.
+ *
+ * http://github.com/pollensoft/archjs
+ *
+ * Much of the inspiration of this project came from Backbone, and it strives to be Backbone compatible as it has many
+ * great features.  But Backbone is also missing a proper inheritance heirarchy, and depends heavily on jQuery and
+ * Underscore which makes it unwieldy on lightweight projects.
+ *
+ * http://www.backbonejs.org
+ *
+ * Keystone is self-contained and is meant to support other frameworks.
  */
 (function( root )
 {
@@ -55,11 +65,12 @@
 			callbacks: {},
 
 			/**
-			 * Add a callback for a specific event to this event dispatcher.
+			 * Add a callback for (a) specific event(s) to this event dispatcher.  Listen to multiple events with the
+			 * same callback by stringing the events together separated by spaces.
 			 *
-			 * @param events String
+			 * @param events String multiple events can be separated by a space to add multiple callbacks.
 			 * @param callback Function
-			 * @param context Object
+			 * @param context Object "this" when callback is executed.
 			 * @return {Keystone.EventDispatcher}
 			 */
 			on: function( events, callback, context )
@@ -69,17 +80,12 @@
 				events = events.split( EventDispatcher.SPLIT_ON );
 				this.callbacks = this.callbacks || {};
 
-				var event, calls, last;
+				var event, callbacks, last, cb;
 				while( event = events.shift() )
 				{
-					calls = this.callbacks[event] = this.callbacks[event] || { last: {} };
+					callbacks = this.callbacks[event] = this.callbacks[event] || [];
 
-					callback = calls.last;
-					callback.next = last = {};
-					callback.context = context;
-					callback.callback = callback;
-
-					calls = { last: last, next: calls ? calls.next : callback };
+					callbacks.push( { callback: callback, context: context } );
 				}
 
 				return this;
@@ -87,8 +93,75 @@
 
 			off: function( events, callback, context )
 			{
+				if( !this.callbacks )
+				{
+					return this;
+				}
 
+				// turn all events off if all parameters are undefined
+				if( !events && !callback && !context )
+				{
+					this.callbacks = undefined;
+					return this;
+				}
+
+				var callbacks, event, cb, idx;
+
+				events = events ? events.split( EventDispatcher.SPLIT_ON ) : this.callbackEvents( callbacks );
+
+				while( event = events.shift() )
+				{
+					callbacks = this.callbacks[event];
+
+					if( !callbacks ) continue;
+
+					// if we have an event, but no callback or context, remove all callbacks for this event
+					if( callbacks && !callback && !context )
+					{
+						this.callbacks[event] = undefined;
+						continue;
+					}
+
+					// loop backwards because we're going to splice out elements as we go
+					idx = callbacks.length;
+					while( idx-- )
+					{
+						cb = callbacks[idx];
+
+						// if either callback or context matches, remove it
+						if( (!callback || callback === cb.callback) && (!context || context === cb.context) )
+						{
+							callbacks.splice( idx, 1 );
+						}
+					}
+				}
+
+			},
+
+			trigger: function()
+			{
+
+			},
+
+			callbackEvents: function( callbacks )
+			{
+				if( Object.keys )
+				{
+					return Object.keys( callbacks );
+				}
+
+				var keys = [], prop;
+				for( prop in callbacks )
+				{
+					if( callbacks.hasOwnProperty( prop ) )
+					{
+						keys.push( prop );
+					}
+				}
+
+				return keys;
 			}
+
 		}
 	);
 
