2ebc08befb67ff83428c3721f586bfeb0e8fcc06
Jay Proulx <jason.proulx@pollensoft.com>
2012-11-21 15:52:49 -0500

    Not yet stable

--- src/main/js/keystone.js
+++ src/main/js/keystone.js
@@ -1,3 +1,10 @@
+/**
+ * Welcome to Keystone!
+ *
+ * Keystone is meant to provide enough code to standardize on implementation patterns, without requiring any other
+ * dependencies.  Using Keystone should allow the developer to standardize on Keystone patterns on a project without
+ * being required to use all of the
+ */
 (function( root )
 {
 	if( !root.Keystone )
@@ -29,26 +36,80 @@
 	 * @param staticProperties (optional) Properties or methods that will be applied directly to the class
 	 * @param localProperties Properties or methods that will be added to the prototype
 	 */
+		// TODO: Implement this method
 	Base.extend = function( staticProperties, localProperties )
 	{
 		localProperties = arguments.length == 1 ? staticProperties : localProperties;
 	};
 
+	/**
+	 * EventDispatcher is a lightweight event dispatcher that can send messages between loosely coupled code.
+	 *
+	 * @type {Keystone.EventDispatcher}
+	 */
+	var EventDispatcher = Keystone.EventDispatcher = Base.extend(
+		{
+			SPLIT_ON: /s+/g
+		},
+		{
+			callbacks: {},
 
-	var ModelBase = Keystone.ModelBase = Base.extend({
+			/**
+			 * Add a callback for a specific event to this event dispatcher.
+			 *
+			 * @param events String
+			 * @param callback Function
+			 * @param context Object
+			 * @return {Keystone.EventDispatcher}
+			 */
+			on: function( events, callback, context )
+			{
+				if( !callback ) return this;
 
-	});
+				events = events.split( EventDispatcher.SPLIT_ON );
+				this.callbacks = this.callbacks || {};
+
+				var event, calls, last;
+				while( event = events.shift() )
+				{
+					calls = this.callbacks[event] = this.callbacks[event] || { last: {} };
+
+					callback = calls.last;
+					callback.next = last = {};
+					callback.context = context;
+					callback.callback = callback;
+
+					calls = { last: last, next: calls ? calls.next : callback };
+				}
+
+				return this;
+			},
+
+			off: function( events, callback, context )
+			{
+
+			}
+		}
+	);
+
+	/**
+	 *
+	 *
+	 * @type {Keystone.ModelBase}
+	 */
+	var ModelBase = Keystone.ModelBase = Base.extend( {
+
+	} );
 
 	/**
 	 * Make Keystone Require.js / AMD friendly.
 	 */
-	if(define && typeof define == "function")
+	if( define && typeof define == "function" )
 	{
-		define("Keystone", [], function()
+		define( "Keystone", [], function()
 		{
 			return root.Keystone;
-		});
+		} );
 	}
-
 
 })( this );
